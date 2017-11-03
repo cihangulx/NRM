cede079df59df1bdf6edf41de1d817d40f949092
qhackci@gmail.com <7411514Cihan>
2017-11-03 14:02:33 +0200

    getProject

--- app/src/main/java/neonyazilim/com/nrm/Fragments/Home/ProjelerFragment.java
+++ app/src/main/java/neonyazilim/com/nrm/Fragments/Home/ProjelerFragment.java
@@ -21,12 +21,13 @@ import neonyazilim.com.nrm.Models.RequestBody;
 import neonyazilim.com.nrm.Models.Talep;
 import neonyazilim.com.nrm.Network.Db;
 import neonyazilim.com.nrm.R;
+import neonyazilim.com.nrm.S;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 /**
- * Created by tuzlabim on 26.10.2017.
+ * Created by cihan on 26.10.2017.
  */
 
 public class ProjelerFragment extends Fragment {
@@ -45,10 +46,38 @@ public class ProjelerFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        getProjeler();
 
     }
     private void getProjeler() {
+        Log.e("responseb","getProjeler();");
+        RequestBody requestBody = new RequestBody();
+        requestBody.setUserId(S.userId);
+        requestBody.setToken(S.userToken);
+        Call<List<Proje>> call = Db.getConnect().getProjeler(requestBody);
+        call.enqueue(new Callback<List<Proje>>() {
+            @Override
+            public void onResponse(Call<List<Proje>> call, Response<List<Proje>> response) {
+                if (response.code()==200){
+                    if (response.body().size() > 0) {
+                        //Projeler Alındı
 
+                        ProjeAdapter projeAdapter = new ProjeAdapter(getActivity(),response.body());
+                        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2, LinearLayoutManager.VERTICAL, false);
+                        recyclerView.setLayoutManager(gridLayoutManager);
+                        recyclerView.setAdapter(projeAdapter);
+
+                        Log.e("responseb",response.body().get(0).getBaslik());
+
+                    }
+                }
+                Log.e("responseb",""+response.code());
+            }
+
+            @Override
+            public void onFailure(Call<List<Proje>> call, Throwable t) {
+                Log.e("responseb",""+t.getMessage());
+            }
+        });
     }
 }
---
+++ app/src/main/java/neonyazilim/com/nrm/Models/Gorev.java
@@ -0,0 +1,8 @@
+package neonyazilim.com.nrm.Models;
+
+/**
+ * Created by tuzlabim on 3.11.2017.
+ */
+
+public class Gorev {
+}
